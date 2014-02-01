0bef411b18dab64fef7f16cc2adc0d592e80142a
gensior <j.franceschini@gmail.com>
2014-02-01 14:42:08 -0800

    Adds NFL teams to NFL controller.

--- public/js/controllers/nfl.js
+++ public/js/controllers/nfl.js
@@ -2,4 +2,7 @@ window.angular.module('ngff.controllers.nfl', [])
   .controller('NFLController', ['$scope', '$routeParams', 'Global', 'NFL',
     function($scope, $routeParams, Global, NFL) {
       $scope.global = Global;
+
+      $scope.nflteams = NFL.teams;
+      $scope.nflteam = NFL.teams[$routeParms['nflTeamId']];
     }]);
