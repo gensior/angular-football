3b31e8f57b047708258c329c809774b913c192d2
gensior <j.franceschini@gmail.com>
2014-02-01 16:51:09 -0800

    Adds league schema.

--- app/models/league.js
+++ app/models/league.js
@@ -2,3 +2,8 @@ var mongoose = require('mongoose'),
     env = process.env.NODE_ENV || 'development',
     config = require('../../config/config')[env],
     Schema = mongoose.Schema;
+
+var LeagueSchema = new Schema({
+  name: {type: String},
+  commissioner: {type: Schema.ObjectId, ref: 'User'}
+});
